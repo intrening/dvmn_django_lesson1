9888e3431a7692d3ba9bfcc564c20d16890e25a8
intrening <wol87@ya.ru>
2021-01-01 16:13:43 +0300

    add places_json in index_view()

--- afisha/views.py
+++ afisha/views.py
@@ -1,4 +1,27 @@
 from django.shortcuts import render
+from places.models import Place
 
 def index_view(request):
-    return render(request, 'index.html')
+    places_json = {
+        "type": "FeatureCollection",
+        "features": [],
+    }
+    for place in Place.objects.all():
+        places_json["features"].append(
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [float(place.lng), float(place.lat)]
+                },
+                "properties": {
+                    "title": place.title,
+                    "placeId": place.pk,
+                    "detailsUrl": "{% static 'places/moscow_legends.json' %}"
+                }
+            },
+        )
+
+    return render(request, 'index.html', context = {
+        'places_json': places_json,
+    })
