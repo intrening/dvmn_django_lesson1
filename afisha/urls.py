93e794a1e9b8d02eca73eb49e5e2bb52853ce61d
intrening <wol87@ya.ru>
2021-01-01 13:37:53 +0300

    Add blank start page

--- afisha/urls.py
+++ afisha/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import main_vew
+
 
 urlpatterns = [
+    path('', main_vew),
     path('admin/', admin.site.urls),
 ]
