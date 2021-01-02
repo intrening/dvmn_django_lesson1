a69d4c66980aa166dd8fa8e5e80016b4a4d74a2a
intrening <wol87@ya.ru>
2021-01-02 15:35:16 +0300

    fix MEDIA_ROOT

--- afisha/settings.py
+++ afisha/settings.py
@@ -119,9 +119,11 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "static"),
-# ]
 
-MEDIA_ROOT = 'media/'
+if not DEBUG:
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, "static"),
+    ]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = 'media/'
